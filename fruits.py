2bc3ccfe3cd353e906c596ca586e088d5d71e15b
GuyMiz95 <guy.mizrahi95@gmail.com>
2024-06-19 03:34:40 +0300

    initialize

--- fruits.py
+++ fruits.py
@@ -1,21 +1,34 @@
+import math
+
 import wikipedia
 import csv
 import json
 from wikipedia import DisambiguationError
-
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+import re
 from tfidf import tfidf
 
 
-def fruitcrawl():
-    with open('fruits.csv', mode='r', newline='') as f:
-        _reader = csv.reader(f)
-        _data = [row for row in _reader]
+# nltk.download('punkt')
+# nltk.download('stopwords')
+
+def fruitcrawl(_data):
+    # with open('fruits.csv', mode='r', newline='') as f:
+    #     _reader = csv.reader(f)
+    #     _data = [row for row in _reader]
     _data = _data[1:]
+    bag_of_words = set()
     for row in _data:
         fruit = row[0]
         fruit_page = get_fruit_page(fruit)
         with open(fruit + '.json', 'w') as f:
             json.dump(fruit_page.content, f)
+            # bag_of_words = bag_of_words.union(get_bag_of_words(fruit_page.content))
+    # return bag_of_words
+
 
 def get_fruit_page(fruit):
     try:
@@ -28,14 +41,103 @@ def get_fruit_page(fruit):
     return page
 
 
+def textsum(tfidf_table, data):
+    return
+
+
+def create_vectors(tfidf_table):
+    sample_word = next(iter(tfidf_table.values()))
+    all_indices = sample_word.keys()
+
+    vectors = {idx: [] for idx in all_indices}
+
+    for word in tfidf_table:
+        for idx in all_indices:
+            vectors[idx].append(tfidf_table[word].get(idx, 0))
+
+    return vectors
+
+
+def get_bag_of_words(content):
+    # Tokenize the text into words
+    words = word_tokenize(content.lower())
+
+    # Convert the list of words into a set to get unique words
+    unique_words = set([word for word in words if word.isalpha()])
+
+    # Print the unique words
+    return unique_words
+
+
+def create_similarity_matrix(sentence_vectors):
+    matrix = [[0 for _ in range(len(sentence_vectors))] for _ in range(len(sentence_vectors))]
+    for row in range(len(sentence_vectors)):
+        for col in range(len(sentence_vectors)):
+            matrix[row][col] = calculate_similarity(sentence_vectors["idx" + str(row)],
+                                                    sentence_vectors["idx" + str(col)])
+    return matrix
+
+
+def calculate_similarity(vector1, vector2):
+    dot_product = calc_dot_product(vector1, vector2)
+    norm_vector1 = math.sqrt(sum(v1 * v1 for v1 in vector1))
+    norm_vector2 = math.sqrt(sum(v2 * v2 for v2 in vector2))
+    return dot_product / (norm_vector1 * norm_vector2)
+
+
+def calc_dot_product(vector1, vector2):
+    dot_product = 0
+    for i in range(len(vector1)):
+        dot_product += vector1[i] * vector2[i]
+    return dot_product
+
+
+def get_highest_similarity_vector_index(similarity_matrix):
+    max_similarity = 0
+    max_similarity_index = 0
+    for i in range(len(similarity_matrix)):
+        if sum(similarity_matrix[i]) > max_similarity:
+            max_similarity = sum(similarity_matrix[i])
+            max_similarity_index = i
+    return max_similarity_index
+
+
+def find_top_k_similar_vectors(similarity_matrix, k=5):
+    # Sum the similarities for each vector
+    similarity_sums = [sum(row) for row in similarity_matrix]
+
+    # Get the indices of the vectors sorted by their summed similarities in descending order
+    sorted_indices = sorted(range(len(similarity_sums)), key=lambda i: similarity_sums[i], reverse=True)
+
+    return sorted_indices[:k]
+
+
 if __name__ == '__main__':
-    festival_terms = ["annual", "music", "festival", "soul", "jazz", "Belgium", "Hungary", "Israel", "Rock", "dance",
-             "Desert", "electronic", "arts"]
+    # read from csv file #
+    # with open('fruits.csv', mode='r', newline='') as f:
+    #     _reader = csv.reader(f)
+    #     _data = [line for line in _reader]
 
-    with open('music_festivals.csv', mode='r', newline='') as file:
-        reader = csv.reader(file)
-        data = [row for row in reader]
+    # create json files for each fruit #
+    # fruitcrawl(_data)
 
-    tfidf(data, festival_terms)
+    # align fruits #
+    # fruits = [line[0] for line in _data[1:]]
 
-    fruitcrawl()
+    # todo: 1) decide if opening json is outside or inside of textsum()
+    # todo: 2) import code into textsum() (i think it should be operated per fruit and return a list of the 5 sentences)
+    # todo: 3) eventually run on all fruits instead of just Banana and we're done with (b)
+    fruits = ["Banana"]
+    for fruit in fruits:
+        with open(fruit + '.json', 'r') as f:
+            cur_data = json.load(f)
+
+        listed_data = sent_tokenize(cur_data)
+        sent_index = ["idx" + str(i) for i in range(len(listed_data))]
+        words_in_fruit = get_bag_of_words(cur_data)
+        fruit_tfidf_score = tfidf(listed_data, words_in_fruit, sent_index)
+        vectors = create_vectors(fruit_tfidf_score)
+        similarity_matrix = create_similarity_matrix(vectors)
+        best_vectors_indexes = find_top_k_similar_vectors(similarity_matrix)
+        for index in best_vectors_indexes:
+            print(listed_data[index])
--- tfidf.py
+++ tfidf.py
@@ -1,33 +1,48 @@
 import csv
 import math
 
+
 def get_tf_td(document, term):
     tf_td = document.lower().count(term.lower())
 
     return tf_td
 
-def tfidf(data, terms):
+
+def tfidf(data, terms, documents_keys):
     # creating vars
-    data = data[1::]
-    documents = [line[0] for line in data]
     num_of_documents = len(data)
 
     # create dictionaries for storing calculated tf-idf data
-    tf_td_dict = {term: {document: 0 for document in documents} for term in terms}
-    dfi_dict = {term : 0 for term in terms}
-    idfi_dict = {term : 0 for term in terms}
-    tf_idf_dict = {term: {document: 0 for document in documents} for term in terms}
+    tf_td_dict = {term: {document: 0 for document in documents_keys} for term in terms}
+    dfi_dict = {term: 0 for term in terms}
+    idfi_dict = {term: 0 for term in terms}
+    tf_idf_dict = {term: {document: 0 for document in documents_keys} for term in terms}
     # iterating input
-    for line in data:
+    for i in range(len(data)):
+        line = data[i]
+        key = documents_keys[i]
         for term in terms:
-            tf_td = get_tf_td(line[-1], term)
-            tf_td_dict[term][line[0]] = tf_td
+            tf_td = get_tf_td(line, term)
+            tf_td_dict[term][key] = tf_td
             # if found in doc - add
             dfi_dict[term] += int(tf_td > 0)
     for term in terms:
         idfi_dict[term] = math.log(num_of_documents / dfi_dict[term], 10)
 
-    for line in data:
+    for key in documents_keys:
         for term in terms:
-            tf_idf_dict[term][line[0]] = tf_td_dict[term][line[0]] * idfi_dict[term]
+            tf_idf_dict[term][key] = tf_td_dict[term][key] * idfi_dict[term]
     return tf_idf_dict
+
+
+if __name__ == '__main__':
+    # initiate music festivals (a)
+    festival_terms = ["annual", "music", "festival", "soul", "jazz", "Belgium", "Hungary", "Israel", "Rock", "dance",
+             "Desert", "electronic", "arts"]
+
+    with open('music_festivals.csv', mode='r', newline='') as f:
+        _reader = csv.reader(f)
+        _data = [row for row in _reader]
+    _data = _data[1:]
+    festival_descriptions = [line[-1] for line in _data]
+    tfidf(festival_descriptions, festival_terms, [line[0] for line in _data])
